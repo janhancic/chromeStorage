7f97c54309060fc2c6684304caa1886d9fdad47d
Jan Hančič <jan.hancic@gmail.com>
2013-02-03 18:53:41 +0100

    refactor

--- src/chrome-storage.js
+++ src/chrome-storage.js
@@ -6,24 +6,51 @@
 	};
 
 	function ChromeStorage ( localStorage ) {
+		var self = this;
+
 		this.localStorage = localStorage;
 
 		this.getItem = bind ( this, this.getItem );
 		this.setItem = bind ( this, this.setItem );
-		this._getKeyType = bind ( this, this._getKeyType );
-		this._getItemByString = bind ( this, this._getItemByString );
-		this._getItemByKeyList = bind ( this, this._getItemByKeyList );
-		this._getItemByDictionary = bind ( this, this._getItemByDictionary );
-	};
 
-	ChromeStorage.prototype._getKeyType = function ( key ) {
-		if ( typeof key === 'string' ) {
-			return 'string';
-		} else if ( Object.prototype.toString.call ( key ) === '[object Array]' ) {
-			return 'array';
-		} else {
-			return 'dictionary';
-		}
+		this._getKeyType = function ( key ) {
+			if ( typeof key === 'string' ) {
+				return 'string';
+			} else if ( Object.prototype.toString.call ( key ) === '[object Array]' ) {
+				return 'array';
+			} else {
+				return 'dictionary';
+			}
+		};
+
+		this._getItemByString = function ( key ) {
+			// TODO: check how chrome handles this (does it return a value or a object { key: value } ?)
+			return self.localStorage.getItem ( key );
+		};
+
+		this._getItemByKeyList = function ( keyList ) {
+			var results = {};
+			for ( k in keyList ) {
+				// TODO: check how chrome handles non existing keys
+				results[keyList[k]] = self.localStorage.getItem ( keyList[k] );
+			}
+
+			return results;
+		};
+
+		this._getItemByDictionary = function ( keyDictionary ) {
+			// get by dictionary
+			var results = {};
+			for ( k in keyDictionary ) {
+				results[k] = self.localStorage.getItem ( k );
+				if ( results[k] === null ) {
+					// use (provided) default value, if key does not exist in storage
+					results[k] = keyDictionary[k];
+				}
+			}
+
+			return results;
+		};
 	};
 
 	ChromeStorage.prototype.getItem = function ( key ) {
@@ -38,35 +65,6 @@
 		}
 
 		throw new TypeError ( 'key is not a string, array or a dictionary' );
-	};
-
-	ChromeStorage.prototype._getItemByString = function ( key ) {
-		// TODO: check how chrome handles this (does it return a value or a object { key: value } ?)
-		return this.localStorage.getItem ( key );
-	};
-
-	ChromeStorage.prototype._getItemByKeyList = function ( keyList ) {
-		var results = {};
-		for ( k in keyList ) {
-			// TODO: check how chrome handles non existing keys
-			results[keyList[k]] = this.localStorage.getItem ( keyList[k] );
-		}
-
-		return results;
-	};
-
-	ChromeStorage.prototype._getItemByDictionary = function ( keyDictionary ) {
-		// get by dictionary
-		var results = {};
-		for ( k in keyDictionary ) {
-			results[k] = this.localStorage.getItem ( k );
-			if ( results[k] === null ) {
-				// use (provided) default value, if key does not exist in storage
-				results[k] = keyDictionary[k];
-			}
-		}
-
-		return results;
 	};
 
 	ChromeStorage.prototype.setItem = function ( key, value ) {
